f6c9862b1a606f676cb8e8289bb8f546eb80f6a5
Devin Shively <devin.shively@gmail.com>
2011-11-24 22:14:35 -0800

    Account model.

---
+++ iOS5/imgurApp/imgurApp/UserAccount.h
@@ -0,0 +1,14 @@
+//
+//  UserAccount.h
+//  imgurApp
+//
+//  Created by Devin Shively on 11/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+// 
+// 
+
+#import <Foundation/Foundation.h>
+
+@interface UserAccount : NSObject
+
+@end
---
+++ iOS5/imgurApp/imgurApp/UserAccount.m
@@ -0,0 +1,13 @@
+//
+//  UserAccount.m
+//  imgurApp
+//
+//  Created by Devin Shively on 11/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserAccount.h"
+
+@implementation UserAccount
+
+@end
